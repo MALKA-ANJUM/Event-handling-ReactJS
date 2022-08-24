788afe0a4e2556ce042535b641bd7e8942ea97a3
Malka <malkaanjum1999@gmail.com>
2022-08-24 12:41:57 +0530

    main coide

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const black = "yellow"
+  const [bg, setBg] = useState(black)
+  const [name, setName] = useState('Click Me')
+  const bgChange = () => {
+    console.log("change");
+    let newBg = "pink"
+    setBg(newBg)
+    setName("Ouch!! 😸")
+  }
+  const bgBack = () =>{
+    setBg("yellow")
+    setName("Ayyo! 😠")
+  }
+
+
   return (
-    <div className="App">
-      
+    <div className="App" style={{background: bg}}>
+        <button onClick={bgChange} onDoubleClick={bgBack}>{name}</button>
     </div>
   );
 }
